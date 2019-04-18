0502716e82500e0eba80834d963d06d7b76e809d
geoffreyli <geoffreyli16@gmail.com>
2019-04-18 12:33:31 -0700

    cleaned up comments and took out diagnostic print statements

--- q1/hw1_q1.py
+++ q1/hw1_q1.py
@@ -16,13 +16,10 @@ sc = SparkContext(conf=conf)
 lines = sc\
     .textFile('/Users/geoffreyli/geoffli @ UW/19-2-Sp-CSE-547/Homeworks/hw1/q1/data/soc-LiveJournal1Adj.txt', 4)\
     .map(lambda x: x.split('\t'))
-print("lines")
-print(lines.take(10))
 
 # Create RDD with (Users, [list of friends])
 # Convert userID to integers, since we'll need to sort numerically later
-# friends = lines.map(lambda u: (u[0], u[1].split(',')))
-
+# Return friends as a list of friend IDs
 
 def cleanLines(l):
     if l[1].strip():
@@ -32,49 +29,27 @@ def cleanLines(l):
 
 
 friends = lines.map(cleanLines)
-print("friends")
-print(friends.take(10))
-
-
-# Create set of all users
-allUsers = friends.keys().collect()
-print("allUsers")
-print(type(allUsers))
-print(len(allUsers))
-
-# Create set of (Users, [list of non-friends])
-# non-friend list is for set of possible friend recommendations, since you only want to recommend non-friends
-# confirmed this created the correct set of non-friends
-# nonfriends = friends.map(lambda x: (x[0], list(set(allusers) - set(x[1]))))
-
-
 
 # Create RDD for all pairs of friends for each user's friend list
 # For each user, create a RDD where:
-# KEY = (Friend1, Friend2) across all combinations of pairs in that user's friend list
-# VALUE = 1
+#   KEY = (Friend1, Friend2) across all combinations of pairs in that user's friend list
+#   VALUE = 1
 friendPairsByUser = friends\
     .map(lambda x: [(x[1][i],x[1][j]) for i in range(len(x[1])) for j in range(i+1, len(x[1]))])\
     .flatMap(lambda x: x)\
     .map(lambda x: (x, 1))
-print("friendPairsByUser")
-print(friendPairsByUser.take(5))
 
+# Need to sort the tuple keys so that (Friend1,Friend2) and (Friend2,Friend1) will group together
 friendPairsByUser = friendPairsByUser.map(lambda x: ((sorted(x[0])[0], sorted(x[0])[1]), 1))
 
-# Now Reduce by grouping on the (Friend1,Friend2) pairs and summing the Values
+# Now Reduce by grouping on the (Friend1,Friend2) tuples and summing the Values
 # The sum of the Values should produce the number of mutual friends between that Friend pair
+# since we created them from the 'perspective' of the User
 mutualFriendsbyFriendPair = friendPairsByUser.reduceByKey(lambda n1, n2: n1 + n2)
-print("mutualFriendsbyFriendPair")
-print(mutualFriendsbyFriendPair.take(5))
-
 
 # Some of the (Friend1, Friend2) pairs are already friends: we need to remove these from our list
 # since we wouldn't want to recommend an existing friend.
-
 friendsDict = friends.collectAsMap()
-print("friendsDict")
-print(friendsDict[0])
 
 def checkIfAlreadyFriends(x):
     user1, user2 = x[0]
@@ -84,44 +59,33 @@ def checkIfAlreadyFriends(x):
         return False
 
 mutualFriendsNonExistingFriendPairs = mutualFriendsbyFriendPair.filter(checkIfAlreadyFriends)
-print("mutualFriendsNonExistingFriendPairs")
-print(mutualFriendsNonExistingFriendPairs.take(5))
 
 
 # Now we need to find the recommended friend lists for each user.
 # Restructure the RDD such that Friend1 becomes the KEY
-# and the VALUE is (Friend2, CountOfMutualFriends)
-
-# friendRecs_part1 = mutualFriendsNonExistingFriendPairs.map(lambda x: (x[0][0], (x[0][1], x[1])))
-# friendRecs_part2 = mutualFriendsNonExistingFriendPairs.map(lambda x: (x[0][1], (x[0][0], x[1])))
-
+# and the VALUE is (Friend2, CountOfMutualFriends with Friend1)
 friendRecs = mutualFriendsNonExistingFriendPairs.map(lambda x: (x[0][0], (x[0][1], x[1])))\
     .union(mutualFriendsNonExistingFriendPairs.map(lambda x: (x[0][1], (x[0][0], x[1]))))\
     .sortByKey()
-
-print("friendRecs")
-print(friendRecs.take(10))
 
 # Map the friendRecs RDD value to a list of tuples (so we can reduceByKey and append list of tuples)
 # ReduceByKey to combine all friend recommendations by userID by appending to list.
 # End result RDD: (UserID, [(FriendRec1, #mutualfriends), (FriendRec2, #mutualfriends), ... ])
 friendRecsByUser = friendRecs.map(lambda x: (x[0], [ x[1] ])).reduceByKey(lambda fr1, fr2: fr1 + fr2)
-print("friendRecsByUser")
-print(friendRecsByUser.take(5))
+
 
 # Now we sort the friendRecsByUser RDD first by descending number of mutual friends, then ascending userID
 friendRecsByUserSorted = friendRecsByUser.map(lambda x: (x[0], sorted(x[1], key=lambda y: (-y[1], y[0]))))
-print("friendRecsByUserSorted")
-print(friendRecsByUserSorted.take(5))
 
+# Sort by UserID Key. Not really necessary but nicer to review output
 friendRecsByUserSorted = friendRecsByUserSorted.sortByKey()
 
+# Test cases. 11 from prompt; 49991 has no friends
 # print(friendRecsByUserSorted.filter(lambda x: x[0] == 11).collect())
 # print(friendRecsByUserSorted.filter(lambda x: x[0] == 49991).collect())
 
 # Extract just the list of friend recommendations for each user (without the # of mutual friends)
 friendRecListByUser = friendRecsByUserSorted.map(lambda x: (x[0], [i[0] for i in x[1]]))
-
 
 # Create function to write output to file
 def write_recs_to_file(finalRecRDD, userList, numRecs, filename):
@@ -131,7 +95,6 @@ def write_recs_to_file(finalRecRDD, userList, numRecs, filename):
     else:
         finalRecDict = finalRecRDD.collectAsMap()
 
-
     with open(filename, 'w') as outfile:
         for user, recs in finalRecDict.items():
             outlen = min(len(recs), numRecs)
@@ -140,7 +103,7 @@ def write_recs_to_file(finalRecRDD, userList, numRecs, filename):
 
 
 # Write all output to file (uncomment below)
-# write_recs_to_file(finalRecRDD=friendRecListByUser, numRecs=10, filename='hw1q1_output_full.txt')
+# write_recs_to_file(finalRecRDD=friendRecListByUser, userList=[], numRecs=10, filename='hw1q1_output_full.txt')
 
 # Write just the requested output for HW1 Q1
 output_IDs = [924, 8941, 8942, 9019, 9020, 9021, 9022, 9990, 9992, 9993]
